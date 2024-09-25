bc412f8c4ccca29d70261f217d351af01d8c3d9f
SanaNaveed311311 <sananaveedahmadawan311311.com>
2024-09-25 06:41:52 -0700

    complete work

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ function addtask() {
     var taskInput = document.getElementById("taskInput");
     var taskList = document.getElementById("taskList");
 
+
     //create the new list item in todo list
     var list = document.createElement("list");
     list.textContent = taskInput.value;
+
 
     /*handling empty task*/
     if(!taskInput.value){
         return alert("you can make an empty list (SORRY)");
     }
+
+
     //Now create the REMOVE button in todo list
     var removebutton = document.createElement("button");
     removebutton.textContent = "REMOVE";
@@ -19,17 +23,21 @@ function addtask() {
         taskList.removeChild(list);
     };
 
+
     //adding click event to mark as completed
     list.oneclick = function () {
         list.classList.toggle("compeleted");
     };
 
+
     // Append the remove button to the list item
     list.appendChild(removebutton);
+
 
     //Apend the list item to the task list
     taskList.appendChild(list);
 
+    
     //clearing the input field
     taskInput.value = '';
 };
